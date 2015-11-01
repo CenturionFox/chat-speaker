3e720128ec7fbf2b0a7677ada82813fd4c90e458
Bridger Maskrey <maskreybe@live.com>
2015-11-01 01:26:23 -0400

    Inital commit

---
+++ MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Attribute.ChatSpeaker
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace Attribute.ChatSpeaker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            var synth = new SpeechSynthesizer();
+
+            var val = synth.GetInstalledVoices();
+
+            foreach (var k in val)
+            {
+                try
+                {
+                    synth.SelectVoice(k.VoiceInfo.Name);
+                    synth.Speak("Hello from " + k.VoiceInfo.Name);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Speak failed for voice " + k.VoiceInfo.Name);
+                }
+            }
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new MainForm());
+        }
+    }
+}
